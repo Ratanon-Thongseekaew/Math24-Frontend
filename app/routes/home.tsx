204788f1f7d977156dcb39441030946096baca52
Ratanon-Thongseekaew <ratanon_thongseekaew@hotmail.com>
2025-05-05 01:52:44 +0700

    complete landing page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Math24" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
